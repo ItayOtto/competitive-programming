4ef915e124d4cc60b526e4fcc7e5597f37ee4ffd
ItayOtto <79222514+ItayOtto@users.noreply.github.com>
2021-03-22 18:33:34 +0200

    Create unique prime factors.cpp

---
+++ Number Theory/Sieve of Eratosthenes/unique prime factors.cpp
@@ -0,0 +1,21 @@
+// make sure to pre-calc this only once!
+// val[x] = #unique prime factors of x
+const int N=2e7+3;
+int val[N]; //this line needs to be in the global area
+vi primes;
+for (int i=2; i<N; ++i) {
+	if (!val[i]){
+    	val[i] = 1;
+    	primes.push_back(i);
+	}
+	for (int p : primes){
+		if (i * p >= N) break;
+		if (i % p == 0){
+			val[i * p] = val[i];
+			break;
+		}
+		else{
+			val[i * p] = val[i] + 1;
+		}
+	}
+}
