5553149293b520fa6fe37b9016d7b443efc4c236
ItayOtto <79222514+ItayOtto@users.noreply.github.com>
2022-03-10 13:04:46 +0200

    Update Fenwick Tree.cpp

--- Data Structures/Fenwick Tree.cpp
+++ Data Structures/Fenwick Tree.cpp
@@ -1,15 +1,27 @@
 struct FT { //1 based indexing
-	ll arr[mxn]={};
-	void update(int pos, ll dif) { // a[pos] += dif
-		for (; pos < mxn; pos += pos&(-pos)) arr[pos] += dif;
+	vll arr;
+	ll mxnsz;
+	FT(int sz){
+		mxnsz = sz + 5;
+		arr=vll(sz + 10, 0);
 	}
+
+	void update(int pos, ll dif) { // a[pos] += dif
+		for (; pos < mxnsz; pos += pos & (-pos)) arr[pos] += dif;
+	}
+
 	ll query(int pos) { // sum of values in [1, pos]
 		ll sm = 0;
 		for (; pos > 0; pos -= pos &(-pos)) sm += arr[pos];
 		return sm;
 	}
+
 	ll range(int l, int r){ // sum of values in [l, r]
-		return query(r)-query(l-1);
+		return query(r) - query(l - 1);
+	}
+
+	ll get(int pos){
+		return range(pos,pos);
 	}
 };
 
